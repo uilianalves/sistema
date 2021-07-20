54b292ad954933a1781be6e0eac98b5a17aec756
uilian <uilianalves@gmail.com>
2021-07-20 12:17:27 -0300

    alt1

--- src/main/java/com/uilian/SistemaApplication.java
+++ src/main/java/com/uilian/SistemaApplication.java
@@ -8,6 +8,7 @@ public class SistemaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaApplication.class, args);
+		System.out.println("ate aqui tudo bem...");
 	}
 
 }
